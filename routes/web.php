440c69d6ed2554c9990e53e5b3c4af591e6df25b
Subhan Raj <shubhanraj2002@gmail.com>
2023-05-16 21:10:00 +0530

    changed route

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,14 @@ use App\Models\Listing;
 // All Listings
 Route::get('/', function(){
     return view('listings', [
-        'heading' => "All Current Listings",
         'listings' => Listing::all()
     ]);
 });
 
 // Single Listing
-
+Route::get('/listings/{listing}', function(Listing $listing){
+    return view('listing', [
+       'listing' => $listing
+    ]);
+});
 
